3647a9a3e3bb264782452682ae61b8aaab2b366e
Mike Bergsma <mike.bergsma@gmail.com>
2014-09-11 10:54:12 -0700

    V3.9.4-140910
    Hypdef.h update

--- code/hypdef.h
+++ code/hypdef.h
@@ -11,8 +11,8 @@
 #define __HYPDEF_H_
 
 #define 	VERSION_HYPERSCRIPT	    "3.9.4"
-#define 	VERSION_BUILD		    "140909"
-#define 	VERSION_HYPERSCRIPT_BUILD   "3.9.4-140909"
+#define 	VERSION_BUILD		    "140910"
+#define 	VERSION_HYPERSCRIPT_BUILD   "3.9.4-140910"
 
 /* Modification history:
  *
