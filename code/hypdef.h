4a55fd30008a9e1023a8444d1d2a3ec745af45be
Mike Bergsma <mike.bergsma@gmail.com>
2015-03-09 17:43:19 -0700

    HS 3.9.4-150223

--- code/hypdef.h
+++ code/hypdef.h
@@ -35,8 +35,8 @@
 #define __HYPDEF_H_
 
 #define 	VERSION_HYPERSCRIPT	    "3.9.4"
-#define 	VERSION_BUILD		    "140910"
-#define 	VERSION_HYPERSCRIPT_BUILD   "3.9.4-140910"
+#define 	VERSION_BUILD		    "150223"
+#define 	VERSION_HYPERSCRIPT_BUILD   "3.9.4-150223"
 
 #define		MAX_STACK_DEPTH		128  		    /* HyperScript stack */
 #define		MAX_EXPRESSION		MAX_STACK_DEPTH * 2 /* Twice the expression length */
